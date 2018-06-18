6433b759c836ff6d97cdc4180ca2e1febfbcde20
Swapnil Patil <swapnilkiran.patil@idemia.com>
2018-06-18 22:48:22 +0530

    Updating the structure

---
+++ src/main/java/springBootApp/welCome.java
@@ -0,0 +1,23 @@
+package springBootApp;
+import org.springframework.boot.*;
+import org.springframework.boot.autoconfigure.*;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@EnableAutoConfiguration
+
+public class welCome {
+
+    @RequestMapping("/")
+    String home() {
+        return "Hello Swapnil !!  Welcome to Backbase Assignment !!";
+    }
+
+    public static void main(String[] args) throws Exception {
+        SpringApplication.run(welCome.class, args);
+    }
+
+
+
+
+}
